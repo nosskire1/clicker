76c674c67822d4dfd584914d7afd37a5e6470d1d
nosskire1 <77719269+nosskire1@users.noreply.github.com>
2022-03-24 10:56:39 +0100

    .

--- js/clicker.js
+++ js/clicker.js
@@ -73,7 +73,7 @@ function step(timestamp) {
     // på samma sätt kan du även dölja uppgraderingar som inte kan köpas
     if (moneyPerClick == 10 && !achievementTest) {
         achievementTest = true;
-        message('Du har hittat en FOSSIL!', 'achievement');
+        message('Killing spree', 'achievement');
     }
 
     window.requestAnimationFrame(step);
@@ -107,17 +107,17 @@ window.addEventListener('load', (event) => {
  */
 upgrades = [
     {
-        name: 'Fin sop',
+        name: 'Sharp teeth',
         cost: 10,
         amount: 1,
     },
     {
-        name: 'Spade',
+        name: 'Ancient magic',
         cost: 100,
         amount: 10,
     },
     {
-        name: 'Hjälpreda',
+        name: 'Cyborg upgrades',
         cost: 1000,
         amount: 100,
     },
@@ -149,18 +149,18 @@ function createCard(upgrade) {
     const cost = document.createElement('p');
 
     header.textContent = `${upgrade.name}, +${upgrade.amount} per sekund.`;
-    cost.textContent = `Köp för ${upgrade.cost} benbitar.`;
+    cost.textContent = `Köp för ${upgrade.cost} corpses.`;
 
     card.addEventListener('click', (e) => {
         if (money >= upgrade.cost) {
             moneyPerClick++;
             money -= upgrade.cost;
             upgrade.cost *= 1.5;
-            cost.textContent = 'Köp för ' + upgrade.cost + ' benbitar';
+            cost.textContent = 'buy for ' + upgrade.cost + ' corpses';
             moneyPerSecond += upgrade.amount;
-            message('Grattis du har lockat till dig fler besökare!', 'success');
+            message('Congratiulations you have raised more undead allias', 'success');
         } else {
-            message('Du har inte råd.', 'warning');
+            message('not enough, kill more', 'warning');
         }
     });
 
